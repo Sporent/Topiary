f707b29313c8f4a268871a94ea92fddcd6cf225a
Sporent <railrider3008@gmail.com>
2023-07-09 00:28:57 +0100

    Initial commit: the sequel

--- src/main/java/net/sporent/topiary/Topiary.java
+++ src/main/java/net/sporent/topiary/Topiary.java
@@ -2,19 +2,22 @@ package net.sporent.topiary;
 
 import net.fabricmc.api.ModInitializer;
 
+import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
+import net.minecraft.block.PillarBlock;
+import net.minecraft.item.BlockItem;
+import net.minecraft.registry.Registries;
+import net.minecraft.registry.Registry;
+import net.minecraft.util.Identifier;
+import net.sporent.topiary.registry.TopiaryBlocks;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class Topiary implements ModInitializer {
-	public static final String MOD_ID = "topiary";
+	public static final String ID = "topiary";
 	public static final Logger LOGGER = LoggerFactory.getLogger("topiary");
 
 	@Override
 	public void onInitialize() {
-		// This code runs as soon as Minecraft is in a mod-load-ready state.
-		// However, some things (like resources) may still be uninitialized.
-		// Proceed with mild caution.
 
-		LOGGER.info("Hello Fabric world!");
 	}
 }
---
+++ src/main/java/net/sporent/topiary/block/KauriSaplingBlock.java
@@ -0,0 +1,13 @@
+package net.sporent.topiary.block;
+
+import net.minecraft.block.SaplingBlock;
+import net.minecraft.block.sapling.SaplingGenerator;
+
+public class KauriSaplingBlock extends SaplingBlock {
+
+    public KauriSaplingBlock(SaplingGenerator generator, Settings settings) {
+        super(generator, settings);
+    }
+
+
+}
