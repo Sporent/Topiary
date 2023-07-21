617b070dbeb4afbb68db80fa1728e9b913de80d5
Sporent <railrider3008@gmail.com>
2023-07-21 17:35:30 +0100

    - rendering kauri sapling and leaves as cutouts

--- src/main/java/net/sporent/topiary/TopiaryClient.java
+++ src/main/java/net/sporent/topiary/TopiaryClient.java
@@ -1,10 +1,14 @@
 package net.sporent.topiary;
 
 import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.fabric.api.blockrenderlayer.v1.BlockRenderLayerMap;
+import net.minecraft.client.render.RenderLayer;
+import net.sporent.topiary.block.TopiaryBlocks;
 
 public class TopiaryClient implements ClientModInitializer {
     @Override
     public void onInitializeClient() {
-
+        BlockRenderLayerMap.INSTANCE.putBlock(TopiaryBlocks.KAURI_LEAVES, RenderLayer.getCutout());
+        BlockRenderLayerMap.INSTANCE.putBlock(TopiaryBlocks.KAURI_SAPLING, RenderLayer.getCutout());
     }
 }
