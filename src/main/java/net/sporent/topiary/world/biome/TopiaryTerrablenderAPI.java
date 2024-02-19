012e6ad101939595ca114c950671ac7bd388b7f7
Sporent <railrider3008@gmail.com>
2024-02-19 22:25:07 +0000

    - temperate rainforest biome is now fully registered

--- src/main/java/net/sporent/topiary/world/biome/TopiaryTerrablenderAPI.java
+++ src/main/java/net/sporent/topiary/world/biome/TopiaryTerrablenderAPI.java
@@ -1,9 +1,13 @@
 package net.sporent.topiary.world.biome;
 
+import net.minecraft.util.Identifier;
+import net.sporent.topiary.Topiary;
+import terrablender.api.Regions;
 import terrablender.api.TerraBlenderApi;
 
 public class TopiaryTerrablenderAPI implements TerraBlenderApi {
     @Override
     public void onTerraBlenderInitialized() {
+        Regions.register(new TopiaryOverworldRegion(new Identifier(Topiary.MOD_ID, "overworld"), 3));
     }
 }
