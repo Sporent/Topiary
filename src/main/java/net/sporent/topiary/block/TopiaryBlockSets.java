0d7562792b250cf2bd05b4fcbd34ff90c5e785b9
Sporent <railrider3008@gmail.com>
2024-04-06 14:30:28 +0100

    left other woodsets unused for now

--- src/main/java/net/sporent/topiary/block/TopiaryBlockSets.java
+++ src/main/java/net/sporent/topiary/block/TopiaryBlockSets.java
@@ -7,10 +7,10 @@ import net.sporent.topiary.Topiary;
 
 public class TopiaryBlockSets {
     public static final BlockSetType KAURI = new BlockSetTypeBuilder().register(new Identifier(Topiary.MOD_ID, "kauri"));
-    public static final BlockSetType VERAWOOD = new BlockSetTypeBuilder().register(new Identifier(Topiary.MOD_ID, "verawood"));
-    public static final BlockSetType WILLOW = new BlockSetTypeBuilder().register(new Identifier(Topiary.MOD_ID, "willow"));
-    public static final BlockSetType COVE = new BlockSetTypeBuilder().register(new Identifier(Topiary.MOD_ID, "cove"));
-    public static final BlockSetType OCEANIC = new BlockSetTypeBuilder().register(new Identifier(Topiary.MOD_ID, "oceanic"));
-    public static final BlockSetType GRUM = new BlockSetTypeBuilder().register(new Identifier(Topiary.MOD_ID, "grum"));
-    public static final BlockSetType PURPLEHEART = new BlockSetTypeBuilder().register(new Identifier(Topiary.MOD_ID, "purpleheart"));
+    //public static final BlockSetType VERAWOOD = new BlockSetTypeBuilder().register(new Identifier(Topiary.MOD_ID, "verawood"));
+    //public static final BlockSetType WILLOW = new BlockSetTypeBuilder().register(new Identifier(Topiary.MOD_ID, "willow"));
+    //public static final BlockSetType COVE = new BlockSetTypeBuilder().register(new Identifier(Topiary.MOD_ID, "cove"));
+    //public static final BlockSetType OCEANIC = new BlockSetTypeBuilder().register(new Identifier(Topiary.MOD_ID, "oceanic"));
+    //public static final BlockSetType GRUM = new BlockSetTypeBuilder().register(new Identifier(Topiary.MOD_ID, "grum"));
+    //public static final BlockSetType PURPLEHEART = new BlockSetTypeBuilder().register(new Identifier(Topiary.MOD_ID, "purpleheart"));
 }
--- src/main/java/net/sporent/topiary/block/TopiaryWoodSets.java
+++ src/main/java/net/sporent/topiary/block/TopiaryWoodSets.java
@@ -7,11 +7,11 @@ import net.sporent.topiary.Topiary;
 
 public class TopiaryWoodSets {
     public static final WoodType KAURI = new WoodTypeBuilder().register(new Identifier(Topiary.MOD_ID, "kauri"), TopiaryBlockSets.KAURI);
-    public static final WoodType VERAWOOD = new WoodTypeBuilder().register(new Identifier(Topiary.MOD_ID, "verawood"), TopiaryBlockSets.VERAWOOD);
-    public static final WoodType WILLOW = new WoodTypeBuilder().register(new Identifier(Topiary.MOD_ID, "willow"), TopiaryBlockSets.WILLOW);
-    public static final WoodType COVE = new WoodTypeBuilder().register(new Identifier(Topiary.MOD_ID, "cove"), TopiaryBlockSets.COVE);
-    public static final WoodType OCEANIC = new WoodTypeBuilder().register(new Identifier(Topiary.MOD_ID, "oceanic"), TopiaryBlockSets.OCEANIC);
-    public static final WoodType GRUM = new WoodTypeBuilder().register(new Identifier(Topiary.MOD_ID, "grum"), TopiaryBlockSets.GRUM);
-    public static final WoodType PURPLEHEART = new WoodTypeBuilder().register(new Identifier(Topiary.MOD_ID, "purpleheart"), TopiaryBlockSets.PURPLEHEART);
+    //public static final WoodType VERAWOOD = new WoodTypeBuilder().register(new Identifier(Topiary.MOD_ID, "verawood"), TopiaryBlockSets.VERAWOOD);
+    //public static final WoodType WILLOW = new WoodTypeBuilder().register(new Identifier(Topiary.MOD_ID, "willow"), TopiaryBlockSets.WILLOW);
+    //public static final WoodType COVE = new WoodTypeBuilder().register(new Identifier(Topiary.MOD_ID, "cove"), TopiaryBlockSets.COVE);
+    //public static final WoodType OCEANIC = new WoodTypeBuilder().register(new Identifier(Topiary.MOD_ID, "oceanic"), TopiaryBlockSets.OCEANIC);
+    //public static final WoodType GRUM = new WoodTypeBuilder().register(new Identifier(Topiary.MOD_ID, "grum"), TopiaryBlockSets.GRUM);
+    //public static final WoodType PURPLEHEART = new WoodTypeBuilder().register(new Identifier(Topiary.MOD_ID, "purpleheart"), TopiaryBlockSets.PURPLEHEART);
 }
 
