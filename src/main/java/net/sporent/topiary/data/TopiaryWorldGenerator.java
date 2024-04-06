cf0ac247f6a535b3326b0aa35c851bedde7d805b
Sporent <railrider3008@gmail.com>
2024-04-06 14:35:32 +0100

    optimised imports

--- src/main/java/net/sporent/topiary/data/TopiaryWorldGenerator.java
+++ src/main/java/net/sporent/topiary/data/TopiaryWorldGenerator.java
@@ -2,7 +2,6 @@ package net.sporent.topiary.data;
 
 import net.fabricmc.fabric.api.datagen.v1.FabricDataOutput;
 import net.fabricmc.fabric.api.datagen.v1.provider.FabricDynamicRegistryProvider;
-import net.minecraft.registry.RegistryKey;
 import net.minecraft.registry.RegistryKeys;
 import net.minecraft.registry.RegistryWrapper;
 import net.sporent.topiary.Topiary;
