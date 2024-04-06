0bafad74fa6a20e199d8215adced6458cb66c54a
Sporent <railrider3008@gmail.com>
2024-04-06 14:36:45 +0100

    add surface rules

--- src/main/java/net/sporent/topiary/world/biome/TopiaryTerrablenderAPI.java
+++ src/main/java/net/sporent/topiary/world/biome/TopiaryTerrablenderAPI.java
@@ -3,11 +3,14 @@ package net.sporent.topiary.world.biome;
 import net.minecraft.util.Identifier;
 import net.sporent.topiary.Topiary;
 import terrablender.api.Regions;
+import terrablender.api.SurfaceRuleManager;
 import terrablender.api.TerraBlenderApi;
 
 public class TopiaryTerrablenderAPI implements TerraBlenderApi {
     @Override
     public void onTerraBlenderInitialized() {
         Regions.register(new TopiaryOverworldRegion(new Identifier(Topiary.MOD_ID, "overworld"), 3));
+
+        SurfaceRuleManager.addSurfaceRules(SurfaceRuleManager.RuleCategory.OVERWORLD, Topiary.MOD_ID, TopiarySurfaceRules.makeRules());
     }
 }
