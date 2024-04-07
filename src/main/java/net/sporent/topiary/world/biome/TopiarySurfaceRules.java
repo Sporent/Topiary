b2f3a6e189cacae837be6ddc265ad883d422f318
Sporent <railrider3008@gmail.com>
2024-04-07 21:21:36 +0100

    added custom surface rule to kauri forests that places podzol at the surface layer using a noise map

--- src/main/java/net/sporent/topiary/world/biome/TopiarySurfaceRules.java
+++ src/main/java/net/sporent/topiary/world/biome/TopiarySurfaceRules.java
@@ -2,39 +2,30 @@ package net.sporent.topiary.world.biome;
 
 import net.minecraft.block.Block;
 import net.minecraft.block.Blocks;
+import net.minecraft.util.math.VerticalSurfaceType;
 import net.minecraft.world.gen.surfacebuilder.MaterialRules;
-import net.sporent.topiary.block.TopiaryBlocks;
+import net.sporent.topiary.world.TopiaryNoiseParameters;
 
 public class TopiarySurfaceRules {
     public static final MaterialRules.MaterialRule PODZOL = makeStateRule(Blocks.PODZOL);
-    public static final MaterialRules.MaterialRule KAURI = makeStateRule(TopiaryBlocks.KAURI_PLANKS);
 
     protected static MaterialRules.MaterialRule makeRules() {
-        //MaterialRules.MaterialCondition podzolNoise = MaterialRules.noiseThreshold(NoiseParametersKeys.PATCH, -0.012, 1.7976931348623157e+308);
-        //MaterialRules.MaterialCondition aboveWater = MaterialRules.water(-1, 0);
-        //MaterialRules.MaterialCondition belowWater = MaterialRules.waterWithStoneDepth(-6, -1);
+        MaterialRules.MaterialCondition podzolNoise = MaterialRules.noiseThreshold(TopiaryNoiseParameters.KAURI_FOREST_SURFACE, -0.43, 0);
+        MaterialRules.MaterialCondition aboveWater = MaterialRules.water(-1, 0);
+        MaterialRules.MaterialCondition surfaceBlock = MaterialRules.stoneDepth(0, false, VerticalSurfaceType.FLOOR);
 
-        //MaterialRules.MaterialRule kauriRule = MaterialRules.condition(MaterialRules.biome(TopiaryBiomes.KAURI_FOREST),
-        //        MaterialRules.condition(belowWater, MaterialRules.sequence(
-        //                MaterialRules.condition(MaterialRules.STONE_DEPTH_FLOOR_WITH_SURFACE_DEPTH, PODZOL),
-        //                MaterialRules.condition(MaterialRules.STONE_DEPTH_FLOOR_WITH_SURFACE_DEPTH_RANGE_6, PODZOL),
-        //                MaterialRules.condition(MaterialRules.stoneDepth(8, true, VerticalSurfaceType.FLOOR), MaterialRules.terracottaBands())
-        //        )));
+        MaterialRules.MaterialRule kauriRule = MaterialRules.sequence(MaterialRules.condition(MaterialRules.biome(TopiaryBiomes.KAURI_FOREST),
+                MaterialRules.condition(
+                        MaterialRules.surface(), MaterialRules.condition(
+                                surfaceBlock, MaterialRules.condition(
+                                        aboveWater, MaterialRules.condition(podzolNoise, PODZOL))))));
 
-        //MaterialRules.MaterialRule kauriRule = MaterialRules.sequence(MaterialRules.condition(MaterialRules.biome(TopiaryBiomes.KAURI_FOREST),
-        //        MaterialRules.condition(podzolNoise, PODZOL)));
-
-        MaterialRules.MaterialCondition isAtOrAboveWaterLevel = MaterialRules.water(-1, 0);
-
-        MaterialRules.MaterialRule grassSurface = MaterialRules.sequence(MaterialRules.condition(isAtOrAboveWaterLevel, KAURI), KAURI);
-
-        return MaterialRules.sequence(
-                MaterialRules.sequence(MaterialRules.condition(MaterialRules.biome(TopiaryBiomes.KAURI_FOREST),
-                                MaterialRules.condition(MaterialRules.STONE_DEPTH_FLOOR, KAURI)),
-                        MaterialRules.condition(MaterialRules.STONE_DEPTH_CEILING, KAURI)));
+        return MaterialRules.sequence(kauriRule);
     }
 
     private static MaterialRules.MaterialRule makeStateRule(Block block){
         return MaterialRules.block(block.getDefaultState());
+
     }
+
 }
